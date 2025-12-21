d727b5b74e133a31026c0102bda00a3dc103bb22
bab3l <bab3l@users.noreply.github.com>
2025-12-21 17:36:08 +0800

    Add cleanup and randomization to console_port and power_port datasource tests

--- internal/datasources_acceptance_tests/console_port_data_source_test.go
+++ internal/datasources_acceptance_tests/console_port_data_source_test.go
@@ -11,14 +11,38 @@ import (
 func TestAccConsolePortDataSource_basic(t *testing.T) {
 
 	t.Parallel()
+
+	siteName := testutil.RandomName("site")
+	siteSlug := testutil.RandomSlug("site")
+	roleName := testutil.RandomName("device-role")
+	roleSlug := testutil.RandomSlug("device-role")
+	mfgName := testutil.RandomName("mfg")
+	mfgSlug := testutil.RandomSlug("mfg")
+	deviceTypeName := testutil.RandomName("device-type")
+	deviceTypeSlug := testutil.RandomSlug("device-type")
+	deviceName := testutil.RandomName("device")
+	portName := testutil.RandomName("console")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterDeviceRoleCleanup(roleSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceCleanup(deviceName)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckSiteDestroy,
+			testutil.CheckDeviceRoleDestroy,
+			testutil.CheckManufacturerDestroy,
+			testutil.CheckDeviceDestroy,
+		),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccConsolePortDataSourceConfig("console0"),
+				Config: testAccConsolePortDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_console_port.test", "name", "console0"),
+					resource.TestCheckResourceAttr("data.netbox_console_port.test", "name", portName),
 					resource.TestCheckResourceAttr("data.netbox_console_port.test", "type", "de-9"),
 					resource.TestCheckResourceAttrSet("data.netbox_console_port.test", "device"),
 				),
@@ -27,31 +51,31 @@ func TestAccConsolePortDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccConsolePortDataSourceConfig(name string) string {
+func testAccConsolePortDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName string) string {
 	return fmt.Sprintf(`
 resource "netbox_site" "test" {
-  name = "Test Site"
-  slug = "test-site"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_role" "test" {
-  name = "Test Device Role"
-  slug = "test-device-role"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_manufacturer" "test" {
-  name = "Test Manufacturer"
-  slug = "test-manufacturer"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_type" "test" {
   manufacturer = netbox_manufacturer.test.id
-  model        = "Test Device Type"
-  slug         = "test-device-type"
+  model        = "%s"
+  slug         = "%s"
 }
 
 resource "netbox_device" "test" {
-  name        = "test-device"
+  name        = "%s"
   device_type = netbox_device_type.test.id
   role        = netbox_device_role.test.id
   site        = netbox_site.test.id
@@ -66,5 +90,5 @@ resource "netbox_console_port" "test" {
 data "netbox_console_port" "test" {
   id = netbox_console_port.test.id
 }
-`, name)
+`, siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName)
 }
--- internal/datasources_acceptance_tests/power_port_data_source_test.go
+++ internal/datasources_acceptance_tests/power_port_data_source_test.go
@@ -1,6 +1,7 @@
 package datasources_acceptance_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bab3l/terraform-provider-netbox/internal/testutil"
@@ -10,14 +11,38 @@ import (
 func TestAccPowerPortDataSource_basic(t *testing.T) {
 
 	t.Parallel()
+
+	siteName := testutil.RandomName("site")
+	siteSlug := testutil.RandomSlug("site")
+	roleName := testutil.RandomName("device-role")
+	roleSlug := testutil.RandomSlug("device-role")
+	mfgName := testutil.RandomName("mfg")
+	mfgSlug := testutil.RandomSlug("mfg")
+	deviceTypeName := testutil.RandomName("device-type")
+	deviceTypeSlug := testutil.RandomSlug("device-type")
+	deviceName := testutil.RandomName("device")
+	portName := testutil.RandomName("power-port")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterDeviceRoleCleanup(roleSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceCleanup(deviceName)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckSiteDestroy,
+			testutil.CheckDeviceRoleDestroy,
+			testutil.CheckManufacturerDestroy,
+			testutil.CheckDeviceDestroy,
+		),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPowerPortDataSourceConfig,
+				Config: testAccPowerPortDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_power_port.test", "name", "Test Power Port"),
+					resource.TestCheckResourceAttr("data.netbox_power_port.test", "name", portName),
 					resource.TestCheckResourceAttr("data.netbox_power_port.test", "type", "iec-60320-c14"),
 				),
 			},
@@ -25,30 +50,31 @@ func TestAccPowerPortDataSource_basic(t *testing.T) {
 	})
 }
 
-const testAccPowerPortDataSourceConfig = `
+func testAccPowerPortDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName string) string {
+	return fmt.Sprintf(`
 resource "netbox_site" "test" {
-  name = "Test Site"
-  slug = "test-site"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_role" "test" {
-  name = "Test Device Role"
-  slug = "test-device-role"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_manufacturer" "test" {
-  name = "Test Manufacturer"
-  slug = "test-manufacturer"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_type" "test" {
   manufacturer = netbox_manufacturer.test.id
-  model        = "Test Device Type"
-  slug         = "test-device-type"
+  model        = "%s"
+  slug         = "%s"
 }
 
 resource "netbox_device" "test" {
-  name        = "test-device"
+  name        = "%s"
   device_type = netbox_device_type.test.id
   role        = netbox_device_role.test.id
   site        = netbox_site.test.id
@@ -56,11 +82,12 @@ resource "netbox_device" "test" {
 
 resource "netbox_power_port" "test" {
   device = netbox_device.test.id
-  name   = "Test Power Port"
+  name   = "%s"
   type   = "iec-60320-c14"
 }
 
 data "netbox_power_port" "test" {
   id = netbox_power_port.test.id
 }
-`
+`, siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, portName)
+}
