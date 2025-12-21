857088ad419ee04836e4389314a9dd8bee5d5da9
bab3l <bab3l@users.noreply.github.com>
2025-12-21 22:08:59 +0800

    Fix notification_group data source test: use randomized name instead of hardcoded constant

--- internal/datasources_acceptance_tests/notification_group_data_source_test.go
+++ internal/datasources_acceptance_tests/notification_group_data_source_test.go
@@ -1,6 +1,7 @@
 package datasources_acceptance_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bab3l/terraform-provider-netbox/internal/testutil"
@@ -11,26 +12,30 @@ func TestAccNotificationGroupDataSource_basic(t *testing.T) {
 
 	t.Parallel()
 
+	name := testutil.RandomName("tf-test-notif-grp")
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccNotificationGroupDataSourceConfig,
+				Config: testAccNotificationGroupDataSourceConfig(name),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_notification_group.test", "name", "Test Notification Group"),
+					resource.TestCheckResourceAttr("data.netbox_notification_group.test", "name", name),
 				),
 			},
 		},
 	})
 }
 
-const testAccNotificationGroupDataSourceConfig = `
+func testAccNotificationGroupDataSourceConfig(name string) string {
+	return fmt.Sprintf(`
 resource "netbox_notification_group" "test" {
-  name = "Test Notification Group"
+  name = %[1]q
 }
 
 data "netbox_notification_group" "test" {
   id = netbox_notification_group.test.id
 }
-`
+`, name)
+}
