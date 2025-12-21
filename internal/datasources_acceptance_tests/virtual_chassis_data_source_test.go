d1b7f9279b6bf5b02f55da0ede2c8ff16c2d299a
bab3l <bab3l@users.noreply.github.com>
2025-12-21 17:21:32 +0800

    Fix virtual_chassis datasource test - remove unsupported cleanup calls

--- internal/datasources_acceptance_tests/virtual_chassis_data_source_test.go
+++ internal/datasources_acceptance_tests/virtual_chassis_data_source_test.go
@@ -1,6 +1,7 @@
 package datasources_acceptance_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bab3l/terraform-provider-netbox/internal/testutil"
@@ -10,25 +11,32 @@ import (
 func TestAccVirtualChassisDataSource_basic(t *testing.T) {
 
 	t.Parallel()
+
+	name := testutil.RandomName("vc")
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-				resource "netbox_virtual_chassis" "test" {
-					name = "test-vc"
-				}
-
-				data "netbox_virtual_chassis" "test" {
-					name = netbox_virtual_chassis.test.name
-				}
-				`,
+				Config: testAccVirtualChassisDataSourceConfig(name),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_virtual_chassis.test", "name", "test-vc"),
+					resource.TestCheckResourceAttr("data.netbox_virtual_chassis.test", "name", name),
 					resource.TestCheckResourceAttrSet("data.netbox_virtual_chassis.test", "id"),
 				),
 			},
 		},
 	})
+}
+
+func testAccVirtualChassisDataSourceConfig(name string) string {
+	return fmt.Sprintf(`
+resource "netbox_virtual_chassis" "test" {
+	name = "%s"
+}
+
+data "netbox_virtual_chassis" "test" {
+	name = netbox_virtual_chassis.test.name
+}
+`, name)
 }
--- internal/datasources_acceptance_tests/vm_interface_data_source_test.go
+++ internal/datasources_acceptance_tests/vm_interface_data_source_test.go
@@ -1,6 +1,7 @@
 package datasources_acceptance_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bab3l/terraform-provider-netbox/internal/testutil"
@@ -10,43 +11,66 @@ import (
 func TestAccVMInterfaceDataSource_basic(t *testing.T) {
 
 	t.Parallel()
+
+	clusterTypeName := testutil.RandomName("cluster-type")
+	clusterTypeSlug := testutil.RandomSlug("cluster-type")
+	clusterName := testutil.RandomName("cluster")
+	vmName := testutil.RandomName("vm")
+	interfaceName := testutil.RandomName("eth")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterClusterTypeCleanup(clusterTypeSlug)
+	cleanup.RegisterClusterCleanup(clusterName)
+	cleanup.RegisterVirtualMachineCleanup(vmName)
+	cleanup.RegisterVMInterfaceCleanup(interfaceName, vmName)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckClusterTypeDestroy,
+			testutil.CheckClusterDestroy,
+			testutil.CheckVirtualMachineDestroy,
+			testutil.CheckVMInterfaceDestroy,
+		),
 		Steps: []resource.TestStep{
 			{
-				Config: `
-				resource "netbox_cluster_type" "test" {
-					name = "test-cluster-type"
-					slug = "test-cluster-type"
-				}
-
-				resource "netbox_cluster" "test" {
-					name = "test-cluster"
-					type = netbox_cluster_type.test.id
-				}
-
-				resource "netbox_virtual_machine" "test" {
-					name    = "test-vm"
-					cluster = netbox_cluster.test.id
-					status  = "active"
-				}
-
-				resource "netbox_vm_interface" "test" {
-					name            = "test-interface"
-					virtual_machine = netbox_virtual_machine.test.name
-				}
-
-				data "netbox_vm_interface" "test" {
-					name            = netbox_vm_interface.test.name
-					virtual_machine = netbox_virtual_machine.test.name
-				}
-				`,
+				Config: testAccVMInterfaceDataSourceConfig(clusterTypeName, clusterTypeSlug, clusterName, vmName, interfaceName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_vm_interface.test", "name", "test-interface"),
+					resource.TestCheckResourceAttr("data.netbox_vm_interface.test", "name", interfaceName),
 					resource.TestCheckResourceAttrSet("data.netbox_vm_interface.test", "id"),
 				),
 			},
 		},
 	})
+}
+
+func testAccVMInterfaceDataSourceConfig(clusterTypeName, clusterTypeSlug, clusterName, vmName, interfaceName string) string {
+	return fmt.Sprintf(`
+resource "netbox_cluster_type" "test" {
+	name = "%s"
+	slug = "%s"
+}
+
+resource "netbox_cluster" "test" {
+	name = "%s"
+	type = netbox_cluster_type.test.id
+}
+
+resource "netbox_virtual_machine" "test" {
+	name    = "%s"
+	cluster = netbox_cluster.test.id
+	status  = "active"
+}
+
+resource "netbox_vm_interface" "test" {
+	name            = "%s"
+	virtual_machine = netbox_virtual_machine.test.name
+}
+
+data "netbox_vm_interface" "test" {
+	name            = netbox_vm_interface.test.name
+	virtual_machine = netbox_virtual_machine.test.name
+}
+`, clusterTypeName, clusterTypeSlug, clusterName, vmName, interfaceName)
 }
