93b45ca82c193741ae0914de579da665239e250f
bab3l <bab3l@users.noreply.github.com>
2025-12-21 17:43:12 +0800

    Add cleanup and randomization to module datasource test

--- internal/datasources_acceptance_tests/module_data_source_test.go
+++ internal/datasources_acceptance_tests/module_data_source_test.go
@@ -1,6 +1,7 @@
 package datasources_acceptance_tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bab3l/terraform-provider-netbox/internal/testutil"
@@ -10,12 +11,39 @@ import (
 func TestAccModuleDataSource_basic(t *testing.T) {
 
 	t.Parallel()
+
+	siteName := testutil.RandomName("site")
+	siteSlug := testutil.RandomSlug("site")
+	roleName := testutil.RandomName("device-role")
+	roleSlug := testutil.RandomSlug("device-role")
+	mfgName := testutil.RandomName("mfg")
+	mfgSlug := testutil.RandomSlug("mfg")
+	deviceTypeName := testutil.RandomName("device-type")
+	deviceTypeSlug := testutil.RandomSlug("device-type")
+	deviceName := testutil.RandomName("device")
+	moduleTypeName := testutil.RandomName("module-type")
+	moduleBayName := testutil.RandomName("module-bay")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterDeviceRoleCleanup(roleSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceTypeCleanup(deviceTypeSlug)
+	cleanup.RegisterDeviceCleanup(deviceName)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckSiteDestroy,
+			testutil.CheckDeviceRoleDestroy,
+			testutil.CheckManufacturerDestroy,
+			testutil.CheckDeviceTypeDestroy,
+			testutil.CheckDeviceDestroy,
+		),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccModuleDataSourceConfig,
+				Config: testAccModuleDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.netbox_module.test", "status", "active"),
 					resource.TestCheckResourceAttrSet("data.netbox_module.test", "device_id"),
@@ -25,30 +53,31 @@ func TestAccModuleDataSource_basic(t *testing.T) {
 	})
 }
 
-const testAccModuleDataSourceConfig = `
+func testAccModuleDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName string) string {
+	return fmt.Sprintf(`
 resource "netbox_site" "test" {
-  name = "Test Site"
-  slug = "test-site"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_role" "test" {
-  name = "Test Device Role"
-  slug = "test-device-role"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_manufacturer" "test" {
-  name = "Test Manufacturer"
-  slug = "test-manufacturer"
+  name = "%s"
+  slug = "%s"
 }
 
 resource "netbox_device_type" "test" {
   manufacturer = netbox_manufacturer.test.id
-  model        = "Test Device Type"
-  slug         = "test-device-type"
+  model        = "%s"
+  slug         = "%s"
 }
 
 resource "netbox_device" "test" {
-  name        = "test-device"
+  name        = "%s"
   device_type = netbox_device_type.test.id
   role        = netbox_device_role.test.id
   site        = netbox_site.test.id
@@ -56,12 +85,12 @@ resource "netbox_device" "test" {
 
 resource "netbox_module_type" "test" {
   manufacturer = netbox_manufacturer.test.id
-  model        = "Test Module Type"
+  model        = "%s"
 }
 
 resource "netbox_module_bay" "test" {
   device = netbox_device.test.id
-  name   = "Test Module Bay"
+  name   = "%s"
 }
 
 resource "netbox_module" "test" {
@@ -74,4 +103,5 @@ resource "netbox_module" "test" {
 data "netbox_module" "test" {
   id = netbox_module.test.id
 }
-`
+`, siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName)
+}
