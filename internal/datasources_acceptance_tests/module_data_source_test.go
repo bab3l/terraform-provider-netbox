5a9c9409d6e8a1599a97afa0b44558af1d51010e
bab3l <bab3l@users.noreply.github.com>
2025-12-24 02:11:27 +0800

    Batch 7: Updated datasources with comprehensive examples and tests

--- internal/datasources_acceptance_tests/module_data_source_test.go
+++ internal/datasources_acceptance_tests/module_data_source_test.go
@@ -53,6 +53,52 @@ func TestAccModuleDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccModuleDataSource_byDeviceAndSerial(t *testing.T) {
+
+	t.Parallel()
+
+	siteName := testutil.RandomName("site")
+	siteSlug := testutil.RandomSlug("site")
+	roleName := testutil.RandomName("device-role")
+	roleSlug := testutil.RandomSlug("device-role")
+	mfgName := testutil.RandomName("mfg")
+	mfgSlug := testutil.RandomSlug("mfg")
+	deviceTypeName := testutil.RandomName("device-type")
+	deviceTypeSlug := testutil.RandomSlug("device-type")
+	deviceName := testutil.RandomName("device")
+	moduleTypeName := testutil.RandomName("module-type")
+	moduleBayName := testutil.RandomName("module-bay")
+	moduleSerial := "TEST-SERIAL-123"
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterDeviceRoleCleanup(roleSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceTypeCleanup(deviceTypeSlug)
+	cleanup.RegisterDeviceCleanup(deviceName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckSiteDestroy,
+			testutil.CheckDeviceRoleDestroy,
+			testutil.CheckManufacturerDestroy,
+			testutil.CheckDeviceTypeDestroy,
+			testutil.CheckDeviceDestroy,
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccModuleDataSourceConfigByDeviceAndSerial(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName, moduleSerial),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.netbox_module.by_serial", "serial", moduleSerial),
+					resource.TestCheckResourceAttrSet("data.netbox_module.by_serial", "device_id"),
+				),
+			},
+		},
+	})
+}
+
 func testAccModuleDataSourceConfig(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName string) string {
 	return fmt.Sprintf(`
 resource "netbox_site" "test" {
@@ -104,4 +150,59 @@ data "netbox_module" "test" {
   id = netbox_module.test.id
 }
 `, siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName)
+}
+
+func testAccModuleDataSourceConfigByDeviceAndSerial(siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName, moduleSerial string) string {
+	return fmt.Sprintf(`
+resource "netbox_site" "test" {
+  name = "%s"
+  slug = "%s"
+}
+
+resource "netbox_device_role" "test" {
+  name = "%s"
+  slug = "%s"
+}
+
+resource "netbox_manufacturer" "test" {
+  name = "%s"
+  slug = "%s"
+}
+
+resource "netbox_device_type" "test" {
+  manufacturer = netbox_manufacturer.test.id
+  model        = "%s"
+  slug         = "%s"
+}
+
+resource "netbox_device" "test" {
+  name        = "%s"
+  device_type = netbox_device_type.test.id
+  role        = netbox_device_role.test.id
+  site        = netbox_site.test.id
+}
+
+resource "netbox_module_type" "test" {
+  manufacturer = netbox_manufacturer.test.id
+  model        = "%s"
+}
+
+resource "netbox_module_bay" "test" {
+  device = netbox_device.test.id
+  name   = "%s"
+}
+
+resource "netbox_module" "test" {
+  device      = netbox_device.test.id
+  module_bay  = netbox_module_bay.test.id
+  module_type = netbox_module_type.test.id
+  serial      = "%s"
+  status      = "active"
+}
+
+data "netbox_module" "by_serial" {
+  device_id = netbox_device.test.id
+  serial    = netbox_module.test.serial
+}
+`, siteName, siteSlug, roleName, roleSlug, mfgName, mfgSlug, deviceTypeName, deviceTypeSlug, deviceName, moduleTypeName, moduleBayName, moduleSerial)
 }
--- internal/datasources_acceptance_tests/module_type_data_source_test.go
+++ internal/datasources_acceptance_tests/module_type_data_source_test.go
@@ -37,6 +37,38 @@ func TestAccModuleTypeDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccModuleTypeDataSource_byModelAndManufacturer(t *testing.T) {
+
+	t.Parallel()
+
+	mfgName1 := testutil.RandomName("mfg1")
+	mfgSlug1 := testutil.RandomSlug("mfg1")
+	mfgName2 := testutil.RandomName("mfg2")
+	mfgSlug2 := testutil.RandomSlug("mfg2")
+	modelName := testutil.RandomName("module-type")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterManufacturerCleanup(mfgSlug1)
+	cleanup.RegisterManufacturerCleanup(mfgSlug2)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy: testutil.ComposeCheckDestroy(
+			testutil.CheckManufacturerDestroy,
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccModuleTypeDataSourceConfigByModelAndManufacturer(mfgName1, mfgSlug1, mfgName2, mfgSlug2, modelName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.netbox_module_type.by_model", "model", modelName),
+					resource.TestCheckResourceAttrSet("data.netbox_module_type.by_model", "manufacturer_id"),
+				),
+			},
+		},
+	})
+}
+
 func testAccModuleTypeDataSourceConfig(mfgName, mfgSlug, modelName string) string {
 	return fmt.Sprintf(`
 resource "netbox_manufacturer" "test" {
@@ -53,4 +85,28 @@ data "netbox_module_type" "test" {
   id = netbox_module_type.test.id
 }
 `, mfgName, mfgSlug, modelName)
+}
+
+func testAccModuleTypeDataSourceConfigByModelAndManufacturer(mfgName1, mfgSlug1, mfgName2, mfgSlug2, modelName string) string {
+	return fmt.Sprintf(`
+resource "netbox_manufacturer" "test1" {
+  name = "%s"
+  slug = "%s"
+}
+
+resource "netbox_manufacturer" "test2" {
+  name = "%s"
+  slug = "%s"
+}
+
+resource "netbox_module_type" "test" {
+  manufacturer = netbox_manufacturer.test1.id
+  model        = "%s"
+}
+
+data "netbox_module_type" "by_model" {
+  model           = netbox_module_type.test.model
+  manufacturer_id = netbox_manufacturer.test1.id
+}
+`, mfgName1, mfgSlug1, mfgName2, mfgSlug2, modelName)
 }
