729ce69455845fc1d37e173535439d13dd8401ed
bab3l <bab3l@users.noreply.github.com>
2025-12-28 10:31:22 +0800

    Test: Add full, update, and import tests to cable_resource_test.go for complete CRUD coverage

--- internal/resources_acceptance_tests/cable_resource_test.go
+++ internal/resources_acceptance_tests/cable_resource_test.go
@@ -88,6 +88,137 @@ func TestAccCableResource_basic(t *testing.T) {
 
 }
 
+func TestAccCableResource_full(t *testing.T) {
+	t.Parallel()
+
+	siteName := testutil.RandomName("test-site-cable-full")
+	siteSlug := testutil.GenerateSlug(siteName)
+	deviceName := testutil.RandomName("test-device-cable-full")
+	mfgName := testutil.RandomName("tf-test-mfg-cable-full")
+	mfgSlug := testutil.GenerateSlug(mfgName)
+	deviceRoleName := testutil.RandomName("tf-test-role-cable-full")
+	deviceRoleSlug := testutil.GenerateSlug(deviceRoleName)
+	deviceTypeModel := testutil.RandomName("tf-test-type-cable-full")
+	deviceTypeSlug := testutil.RandomSlug("device-type-full")
+	interfaceNameA := testutil.RandomName("eth")
+	interfaceNameB := testutil.RandomName("eth")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceRoleCleanup(deviceRoleSlug)
+	cleanup.RegisterDeviceTypeCleanup(deviceTypeSlug)
+	cleanup.RegisterDeviceCleanup(deviceName + "-a")
+	cleanup.RegisterDeviceCleanup(deviceName + "-b")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCableResourceConfig_full(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("netbox_cable.test", "id"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "status", "connected"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "type", "cat6"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "description", testutil.Description1),
+					resource.TestCheckResourceAttr("netbox_cable.test", "comments", testutil.Comments),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCableResource_update(t *testing.T) {
+	t.Parallel()
+
+	siteName := testutil.RandomName("test-site-cable-update")
+	siteSlug := testutil.GenerateSlug(siteName)
+	deviceName := testutil.RandomName("test-device-cable-update")
+	mfgName := testutil.RandomName("tf-test-mfg-cable-update")
+	mfgSlug := testutil.GenerateSlug(mfgName)
+	deviceRoleName := testutil.RandomName("tf-test-role-cable-update")
+	deviceRoleSlug := testutil.GenerateSlug(deviceRoleName)
+	deviceTypeModel := testutil.RandomName("tf-test-type-cable-update")
+	deviceTypeSlug := testutil.RandomSlug("device-type-update")
+	interfaceNameA := testutil.RandomName("eth")
+	interfaceNameB := testutil.RandomName("eth")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceRoleCleanup(deviceRoleSlug)
+	cleanup.RegisterDeviceTypeCleanup(deviceTypeSlug)
+	cleanup.RegisterDeviceCleanup(deviceName + "-a")
+	cleanup.RegisterDeviceCleanup(deviceName + "-b")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCableResourceConfig(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_cable.test", "status", "connected"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "type", "cat6"),
+				),
+			},
+			{
+				Config: testAccCableResourceConfig_withDescription(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_cable.test", "status", "connected"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "type", "cat6"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "description", testutil.Description2),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCableResource_import(t *testing.T) {
+	t.Parallel()
+
+	siteName := testutil.RandomName("test-site-cable-imp")
+	siteSlug := testutil.GenerateSlug(siteName)
+	deviceName := testutil.RandomName("test-device-cable-imp")
+	mfgName := testutil.RandomName("tf-test-mfg-cable-imp")
+	mfgSlug := testutil.GenerateSlug(mfgName)
+	deviceRoleName := testutil.RandomName("tf-test-role-cable-imp")
+	deviceRoleSlug := testutil.GenerateSlug(deviceRoleName)
+	deviceTypeModel := testutil.RandomName("tf-test-type-cable-imp")
+	deviceTypeSlug := testutil.RandomSlug("device-type-imp")
+	interfaceNameA := testutil.RandomName("eth")
+	interfaceNameB := testutil.RandomName("eth")
+
+	cleanup := testutil.NewCleanupResource(t)
+	cleanup.RegisterSiteCleanup(siteSlug)
+	cleanup.RegisterManufacturerCleanup(mfgSlug)
+	cleanup.RegisterDeviceRoleCleanup(deviceRoleSlug)
+	cleanup.RegisterDeviceTypeCleanup(deviceTypeSlug)
+	cleanup.RegisterDeviceCleanup(deviceName + "-a")
+	cleanup.RegisterDeviceCleanup(deviceName + "-b")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCableResourceConfig(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("netbox_cable.test", "id"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "status", "connected"),
+					resource.TestCheckResourceAttr("netbox_cable.test", "type", "cat6"),
+				),
+			},
+			{
+				ResourceName:      "netbox_cable.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func TestAccCableResource_IDPreservation(t *testing.T) {
 	t.Parallel()
 
@@ -244,6 +375,252 @@ resource "netbox_cable" "test" {
 }
 
 `, siteName, siteSlug, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, deviceName, interfaceNameA, interfaceNameB)
+
+}
+
+func testAccCableResourceConfig_full(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB string) string {
+
+	return fmt.Sprintf(`
+
+resource "netbox_site" "test" {
+
+  name = %[1]q
+
+  slug = %[2]q
+
+  status = "active"
+
+}
+
+resource "netbox_manufacturer" "test" {
+
+  name = %[3]q
+
+  slug = %[4]q
+
+}
+
+resource "netbox_device_role" "test" {
+
+  name = %[5]q
+
+  slug = %[6]q
+
+}
+
+resource "netbox_device_type" "test" {
+
+  model = %[7]q
+
+  slug  = %[8]q
+
+  manufacturer = netbox_manufacturer.test.id
+
+}
+
+resource "netbox_device" "test_a" {
+
+  name           = "%[9]s-a"
+
+  device_type    = netbox_device_type.test.id
+
+  role           = netbox_device_role.test.id
+
+  site           = netbox_site.test.id
+
+}
+
+resource "netbox_device" "test_b" {
+
+  name           = "%[9]s-b"
+
+  device_type    = netbox_device_type.test.id
+
+  role           = netbox_device_role.test.id
+
+  site           = netbox_site.test.id
+
+}
+
+resource "netbox_interface" "test_a" {
+
+  name      = %[10]q
+
+  device    = netbox_device.test_a.id
+
+  type      = "1000base-t"
+
+}
+
+resource "netbox_interface" "test_b" {
+
+  name      = %[11]q
+
+  device    = netbox_device.test_b.id
+
+  type      = "1000base-t"
+
+}
+
+resource "netbox_cable" "test" {
+
+  status      = "connected"
+
+  type        = "cat6"
+
+  description = %q
+
+  comments    = %q
+
+  a_terminations = [
+
+    {
+
+      object_type = "dcim.interface"
+
+      object_id   = netbox_interface.test_a.id
+
+    }
+
+  ]
+
+  b_terminations = [
+
+    {
+
+      object_type = "dcim.interface"
+
+      object_id   = netbox_interface.test_b.id
+
+    }
+
+  ]
+
+}
+
+`, siteName, siteSlug, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, deviceName, interfaceNameA, interfaceNameB, testutil.Description1, testutil.Comments)
+
+}
+
+func testAccCableResourceConfig_withDescription(siteName, siteSlug, deviceName, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, interfaceNameA, interfaceNameB string) string {
+
+	return fmt.Sprintf(`
+
+resource "netbox_site" "test" {
+
+  name = %[1]q
+
+  slug = %[2]q
+
+  status = "active"
+
+}
+
+resource "netbox_manufacturer" "test" {
+
+  name = %[3]q
+
+  slug = %[4]q
+
+}
+
+resource "netbox_device_role" "test" {
+
+  name = %[5]q
+
+  slug = %[6]q
+
+}
+
+resource "netbox_device_type" "test" {
+
+  model = %[7]q
+
+  slug  = %[8]q
+
+  manufacturer = netbox_manufacturer.test.id
+
+}
+
+resource "netbox_device" "test_a" {
+
+  name           = "%[9]s-a"
+
+  device_type    = netbox_device_type.test.id
+
+  role           = netbox_device_role.test.id
+
+  site           = netbox_site.test.id
+
+}
+
+resource "netbox_device" "test_b" {
+
+  name           = "%[9]s-b"
+
+  device_type    = netbox_device_type.test.id
+
+  role           = netbox_device_role.test.id
+
+  site           = netbox_site.test.id
+
+}
+
+resource "netbox_interface" "test_a" {
+
+  name      = %[10]q
+
+  device    = netbox_device.test_a.id
+
+  type      = "1000base-t"
+
+}
+
+resource "netbox_interface" "test_b" {
+
+  name      = %[11]q
+
+  device    = netbox_device.test_b.id
+
+  type      = "1000base-t"
+
+}
+
+resource "netbox_cable" "test" {
+
+  status      = "connected"
+
+  type        = "cat6"
+
+  description = %q
+
+  a_terminations = [
+
+    {
+
+      object_type = "dcim.interface"
+
+      object_id   = netbox_interface.test_a.id
+
+    }
+
+  ]
+
+  b_terminations = [
+
+    {
+
+      object_type = "dcim.interface"
+
+      object_id   = netbox_interface.test_b.id
+
+    }
+
+  ]
+
+}
+
+`, siteName, siteSlug, mfgName, mfgSlug, deviceRoleName, deviceRoleSlug, deviceTypeModel, deviceTypeSlug, deviceName, interfaceNameA, interfaceNameB, testutil.Description2)
 
 }
 
